1645746964e6aa5a8861702d8addd9c194f05bef
Andrey Vassilev <avassilev@gmail.com>
2019-02-10 20:30:08 +0200

    Estimation code and data

---
+++ Code/NKM_est_gap_v1.mod
@@ -0,0 +1,49 @@
+var y trend_y cycle_y Pi betta;
+
+varexo v1 v2 v3 v4 ;
+
+parameters c1 c2 c3 c4;
+c1 = 0.7;
+c2 = 0.5;
+c3 = 0.01;
+c4 = 1;
+
+
+model(linear);
+
+y = trend_y + cycle_y;
+trend_y = trend_y(-1) + betta(-1) + v1;
+betta = betta(-1) + v2;
+cycle_y = c1*cycle_y(-1) + v3;
+
+Pi = (1-c2)*c3 + c2*Pi(-1) + c4*cycle_y + v4;
+
+
+end;
+
+initval;
+y = 8.001577;
+trend_y = y;
+betta = 0;
+cycle_y  = 0;
+Pi = c3;
+end;
+
+
+steady(nocheck);
+
+estimated_params;
+stderr v1     , inv_gamma_pdf,  0.005  , inf;
+stderr v2     , inv_gamma_pdf,  0.005  , inf;
+stderr v3     , inv_gamma_pdf,  0.005  , inf;
+stderr v4     , inv_gamma_pdf,  0.005  , inf;
+
+c1        , normal_pdf   ,  0.7    , 0.03;
+c2        , beta_pdf     ,  0.5    , 0.2;
+c3        , normal_pdf   ,  0.01   , 0.01;
+c4        , normal_pdf   ,  1      , 1;
+end;
+
+varobs y Pi; 
+
+estimation(datafile=estdata,mh_replic=2000, mode_compute=4, mh_nblocks=2, mh_jscale=0.3, filtered_vars, smoother, diffuse_filter) trend_y cycle_y Pi; 
---
+++ Code/PrepareEstData.R
@@ -0,0 +1,59 @@
+# library imports ---------------------------------------------------------
+
+# Set working directory to script location
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+library(alfred)
+library(tidyverse)
+library(lubridate)
+
+
+# import data -------------------------------------------------------------
+
+gdp <- get_fred_series("GDPC1") #  Billions of Chained 2012 Dollars, Seasonally Adjusted
+core_cpi <- get_fred_series("CPILFESL") # Consumer Price Index for All Urban Consumers: All Items Less Food & Energy
+                                         # Index 1982-1984=100, Seasonally Adjusted 
+
+save(gdp,core_cpi, file = "FREDdata.RData")
+
+
+# load data ---------------------------------------------------------------
+
+load("FREDdata.RData")
+
+# transform data ----------------------------------------------------------
+
+lgdp <- gdp %>% 
+  mutate(qtr = quarter(date,with_year = TRUE),
+         lgdp = log(GDPC1)) %>% 
+  select(qtr, lgdp)
+
+core_infl <- core_cpi %>% 
+  mutate(qtr = quarter(date,with_year = TRUE)) %>% 
+  group_by(qtr) %>% 
+  summarise(avg_core_cpi = mean(CPILFESL)) %>% 
+  mutate(core_infl = avg_core_cpi/lag(avg_core_cpi)-1) %>% 
+  select(qtr, core_infl)
+
+dta <- lgdp %>% 
+  left_join(core_infl, by = "qtr") %>% 
+  na.omit()
+
+
+# export to m file --------------------------------------------------------
+
+
+fname <- "estdata.m"
+
+write("dta = [", fname)
+
+write.table(dta, file = fname, append = TRUE, quote = FALSE, sep = " ",
+            eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+            col.names = FALSE)
+
+write("];
+
+y = dta(:,2);
+Pi = dta(:,3);
+
+clear dta;", fname, append = TRUE)
